f67cd8dd5cfac4cbb84f742fa5dd0d84278d7c99
nvpost <nvbpost@yandex.ru>
2022-08-09 14:18:51 +0300

    add main component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld :mes="mes" />
+    <Main :mes="mes" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Main from './components/Main.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    Main,
   },
   data() {
     return {
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -6,7 +6,7 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Main',
   props: {
     msg: String,
     mes: String,
