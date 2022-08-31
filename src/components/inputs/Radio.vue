3b3109b06b5d00c7424e13c9ae05f85e69b90873
nvpost <nvbpost@yandex.ru>
2022-08-31 12:43:26 +0300

    close_inner_radio

--- src/components/inputs/Radio.vue
+++ src/components/inputs/Radio.vue
@@ -1,25 +1,27 @@
 <template>
   <div class="input_item">
     <h4>{{ question.id }}) {{ question.label }}</h4>
-    {{ question.answ.values.options }}
-    <template v-for="(val, val_index) in question.answ.values[0].options">
 
-    <RadioItemValue :id="'val' + question.id" 
-    :label="val"
-    :checked="val_index == question.answ.values[0].default_id ? 'checked': false"
-    >
+    <template v-for="(val, val_index) in question.answ.values[0].options">
+      <div class="input_item_value">
+        <input
+          type="Radio"
+          :id="'val' + question.id"
+          :name="'val' + question.id"
+          :checked="
+            val_index == question.answ.values[0].default_id ? 'checked' : false
+          "
+        />
+        <label :for="'val' + question.id">{{ val }}</label>
+      </div>
     </template>
   </div>
 </template>
 
 <script>
-import RadioItemValue from './RadioItemValue';
-
 export default {
   name: 'Radio',
-  components: {
-    RadioItemValue
-  },
+  components: {},
   props: ['question'],
 };
 </script>
